4c377d727a82f66edcba252203eb194dc017dd19
Suvam Chauhan <chauhansuvam@gmail.com>
2024-04-20 20:26:55 -0400

    finishing touch

--- js/sprite.js
+++ js/sprite.js
@@ -1,33 +1,19 @@
-// Thanks to http://www.codesynthesis.co.uk/code-snippets/move-an-element-with-arrow-keys-using-jquery
 $(document).ready(function(){
-	/* STEP 6a: Capture the keydown event for moving the sprite */
- document.addEventListener("keydown", (event) => { 
-	var KeyCode = event.code;
-	console.log(KeyCode); 
-	/* STEP 8a: Build out the cases for each of the arrow keys */
- switch(KeyCode){
- case "ArrowUp": 
- document.getElementById('dude').className = "up";
- break;
- case "ArrowDown": 
- document.getElementById('dude').className= "down"; 
- break;
- case "ArrowLeft": 
- document.getElementById('dude').className="left";  
- break;
- case "ArrowRight": 		 
- document.getElementById('dude').className="right"; 
- break;
- }
-	/* STEP 8b: Return false so keys don't return anything */
-return false; 
-});
+    $(document).on('keydown', function(event) {
+        var keyCode = event.code;
+        var character = $('#CharacterSprite');
+        if (keyCode === 'ArrowUp') {
+            moveCharacter(character, 'up');
+        } else if (keyCode === 'ArrowDown') {
+            moveCharacter(character, 'down');
+        } else if (keyCode === 'ArrowRight') {
+            moveCharacter(character, 'right');
+        } else if (keyCode === 'ArrowLeft') {
+            moveCharacter(character, 'left');
+        }
+    });
 
-document.addEventListener("keyup", ()=>{
-	document.getElementById('dude').className='go';
-	});
-	/* STEP 6b: Close out the keydown event anonymous function */
-	/* STEP 7a: Capture the keyup event to stop the sprite */
-	/* STEP 7b: Close out the keyup event anonymous function */
-
+    function moveCharacter(character, direction) {
+        character.removeClass().addClass("CharacterSprite " + direction);
+    }
 });
